d06d70cb9975b5384346272277e71ecf33d422fd
ayseboogie <ayse.stinnett@gmail.com>
2018-08-31 19:12:12 -0400

    routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,71 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModulesPro} from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader} from 'ng-uikit-pro-standard';
 
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
+import { FamiliesComponent } from './families/families.component';
+import { HomeComponent } from './home/home.component';
+import { MusiciansComponent } from './musicians/musicians.component';
+import { OutdoorsComponent } from './outdoors/outdoors.component';
+import { PortraitsComponent } from './portraits/portraits.component';
+import { RealestateComponent } from './realestate/realestate.component';
+import { TravelsComponent } from './travels/travels.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { AppRoutingModule} from './app.routing';
+import {Routes, RouterModule} from '@angular/router';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+import { 'hammerjs'};
+import { 'mousetrap'};
+
+const appRoutes: Routes = [
+  { path: 'dashboard',      component: HomeComponent },
+  { path: 'aboutme',      component: AboutmeComponent },
+  { path: 'families',      component: FamiliesComponent },
+  { path: 'musicians',      component: MusiciansComponent },
+  { path: 'outdoors',      component: OutdoorsComponent },
+  { path: 'portrait',      component: PortraitsComponent },
+  { path: 'realestate',      component: RealestateComponent },
+  { path: 'travels',      component: TravelsComponent },
+  { path: 'weddings',      component: WeddingsComponent },
+  { path: 'contact',      component: ContactComponent },
+  { path: '',          redirectTo: 'dashboard', pathMatch: 'full' }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ContactComponent,
+    FamiliesComponent,
+    HomeComponent,
+    MusiciansComponent,
+    OutdoorsComponent,
+    PortraitsComponent,
+    RealestateComponent,
+    TravelsComponent,
+    WeddingsComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModulesPro.forRoot(),
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MDBSpinningPreloader],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import {AboutmeComponent} from './aboutme/aboutme.component';
+import {FamiliesComponent} from './families/families.component';
+import {MusiciansComponent} from './musicians/musicians.component';
+import {OutdoorsComponent} from './outdoors/outdoors.component';
+import {PortraitsComponent} from './portraits/portraits.component';
+import {RealestateComponent} from './realestate/realestate.component';
+import {TravelsComponent} from './travels/travels.component';
+import {WeddingsComponent} from './weddings/weddings.component';
+import {ContactComponent} from './contact/contact.component';
+
+
+const routes: Routes = [
+  { path: 'dashboard',      component: HomeComponent },
+  { path: 'aboutme',      component: AboutmeComponent },
+  { path: 'families',      component: FamiliesComponent },
+  { path: 'musicians',      component: MusiciansComponent },
+  { path: 'outdoors',      component: OutdoorsComponent },
+  { path: 'portrait',      component: PortraitsComponent },
+  { path: 'realestate',      component: RealestateComponent },
+  { path: 'travels',      component: TravelsComponent },
+  { path: 'weddings',      component: WeddingsComponent },
+  { path: 'contact',      component: ContactComponent },
+  { path: '',          redirectTo: 'dashboard', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
